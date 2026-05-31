a21e32facf85ace51c2ff68601ec15ff1d703fc7
Le Dinh Tan <tanld996@gmail.com>
2026-05-31 17:01:55 +0200

    updated utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,8 @@
 import os
 import sys
-
-import numpy as np
-import pandas as pd
 import pickle
 
-from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+import yaml
 
 from src.exception import CustomException
 
@@ -22,41 +18,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models, param):
-    try:
-        report = {}
-
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-
-            para=param[list(models.keys())[i]]
-
-            gs = GridSearchCV(model, para, cv=3)
-            gs.fit(X_train, y_train)
-
-            model.set_params(**gs.best_params_)
-
-            model.fit(X_train,y_train)
-
-            y_train_pred = model.predict(X_train)
-
-            y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
-
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
-
-        return report
-
-    except Exception as e:
-        raise CustomException(e, sys)
-    
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
             return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def read_yaml(file_path):
+    try:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"YAML configuration file not found: {file_path}")
+
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
 
     except Exception as e:
         raise CustomException(e, sys)
