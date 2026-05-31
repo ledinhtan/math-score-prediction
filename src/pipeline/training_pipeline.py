1ff672cf3675e2eada353212bf9274d6158efc26
Le Dinh Tan <tanld996@gmail.com>
2026-05-31 17:02:40 +0200

    updated training pipeline

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,42 +1,51 @@
 import sys
 
+import mlflow
+
+from src.config.configuration import ConfigurationManager
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 from src.exception import CustomException
 from src.logger import logging
 
+
 class TrainingPipeline:
     def __init__(self):
-        self.data_ingestion = DataIngestion()
-        self.data_transformation = DataTransformation()
-        self.model_trainer = ModelTrainer()
+        self.config = ConfigurationManager()
+        self.data_ingestion = DataIngestion(self.config.data_ingestion_config)
+        self.data_transformation = DataTransformation(self.config.data_transformation_config)
+        self.model_trainer = ModelTrainer(self.config.model_trainer_config)
 
     def start_training_pipeline(self):
         try:
-            logging.info('Starting training pipeline.')
+            logging.info("Starting training pipeline.")
+            mlflow.set_tracking_uri(self.config.model_trainer_config.tracking_uri)
+            mlflow.set_experiment(self.config.model_trainer_config.experiment_name)
 
-            train_data_path, test_data_path = self.data_ingestion.initiate_data_ingestion()
+            with mlflow.start_run(run_name="training_pipeline"):
+                train_data_path, test_data_path = self.data_ingestion.initiate_data_ingestion()
 
-            train_arr, test_arr, _ = self.data_transformation.initiate_data_transformation(
-                train_path=train_data_path,
-                test_path=test_data_path
-            )
+                train_arr, test_arr, preprocessor_path = self.data_transformation.initiate_data_transformation(
+                    train_path=train_data_path,
+                    test_path=test_data_path,
+                )
 
-            training_result = self.model_trainer.initiate_model_trainer(
-                train_array=train_arr,
-                test_array=test_arr
-            )
+                mlflow.log_artifact(preprocessor_path)
 
-            logging.info('Training pipeline completed successfully.')
+                training_result = self.model_trainer.initiate_model_trainer(
+                    train_array=train_arr,
+                    test_array=test_arr,
+                )
 
-            return training_result
+                logging.info("Training pipeline completed successfully.")
+                return training_result
 
         except Exception as e:
             logging.error(CustomException(e, sys))
             raise CustomException(e, sys)
 
+
 if __name__ == '__main__':
     pipeline = TrainingPipeline()
-    result = pipeline.start_training_pipeline()
-    print(result)
+    print(pipeline.start_training_pipeline())
